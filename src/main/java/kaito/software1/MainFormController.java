aa5e757f9a70cef0ae7d5f0e8764431d086c0027
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-05-19 19:13:41 +0200

    implemented search for parts

--- src/main/java/kaito/software1/MainFormController.java
+++ src/main/java/kaito/software1/MainFormController.java
@@ -1,24 +1,32 @@
 package kaito.software1;
 
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static kaito.software1.Part.partList;
+
 public class MainFormController implements Initializable {
+    @FXML
+    private TextField searchPart;
+    @FXML
+    private TextField searchProduct;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -46,8 +54,34 @@ public class MainFormController implements Initializable {
         partName.setCellValueFactory(new PropertyValueFactory<>("PartName"));
         partInv.setCellValueFactory(new PropertyValueFactory<>("PartInv"));
         partPrice.setCellValueFactory(new PropertyValueFactory<>("PartPrice"));
-        partsTable.setItems(Part.partList);
+        partsTable.setItems(partList);
+
+        FilteredList<Part> filteredParts = new FilteredList<>(partList, p -> true);
+        searchPart.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            filteredParts.setPredicate(part -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                
+                String lowerCaseFilter = newValue.toLowerCase();
+
+                if (Integer.toString(part.getPartId()).toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+                } else if (part.getPartName().toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+                }
+                return false;
+            });
+        });
+
+        SortedList<Part> sortedParts = new SortedList<>(filteredParts);
+
+        sortedParts.comparatorProperty().bind(partsTable.comparatorProperty());
+
+        partsTable.setItems(sortedParts);
+
     }
+
 
     public void addPart(ActionEvent actionEvent) throws IOException {
         root = FXMLLoader.load(getClass().getResource("add-part-form.fxml"));
